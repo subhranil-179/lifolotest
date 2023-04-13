51ecfe0bf4be580f03f0c9ff0114ff17ee205bba
subhranil <subhranil@148.166.178.68.host.secureserver.net>
2023-04-13 15:01:39 +0000

    rest local branch

--- humanlifology/settings.py
+++ humanlifology/settings.py
@@ -18,7 +18,7 @@ import dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv.read_dotenv(os.path.join(BASE_DIR, ".env"))
 
-DEBUG = True
+DEBUG = False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
